4d384dc45a870cde195c8989a0d1b67186cd874e
payth1 <59085690+remoski@users.noreply.github.com>
2023-05-10 09:09:10 +0200

    Project setup, most import included

---
+++ src/main/java/com/project/QAserver/PostManager.java
@@ -0,0 +1,17 @@
+package com.project.QAserver;
+
+import org.json.JSONObject;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+
+public class PostManager {
+
+}
---
+++ src/main/java/com/project/QAserver/QAserverApplication.java
@@ -0,0 +1,19 @@
+package com.project.QAserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class QAserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAserverApplication.class, args);
+	}
+
+}
