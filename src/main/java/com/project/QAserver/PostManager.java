b5fa985768f516b4c1964c86b69b0c42e5e53d24
payth1 <59085690+remoski@users.noreply.github.com>
2023-05-15 14:46:47 +0200

    Listed all the necessary functions, and request maps, created test question JSON files as well as function to generate them if more are needed

--- src/main/java/com/project/QAserver/PostManager.java
+++ src/main/java/com/project/QAserver/PostManager.java
@@ -10,17 +10,63 @@ import java.util.ArrayList;
 
 import java.util.Arrays;
 import java.util.UUID;
+/*
+Format for JSON:
 
+{"id":"[ID],"question":"[question","answers":[{"username":"[username]","answer":"[answer]"}]}
+
+ */
 
 public class PostManager {
-    public String test(){
-        File temp = new File("src/main/resources/postJSON");
-        JSONObject testJSON = new JSONObject();
-        testJSON.put("ID", UUID.randomUUID());
-        testJSON.put("question","question 1");
-        ArrayList<JSONObject> jsonArrayList = new ArrayList<JSONObject>();
-        testJSON.put("answers", jsonArrayList);
 
-        return testJSON.toString();
+    public void listablePost(int page){
+
+    }
+    public void addPost(){
+
+    }
+
+    public void addAnswer(){
+
+    }
+
+//  USE THE FOLLOWING CODE TO CREATE TEST QUESTIONS
+    public void setupTest(){
+        JSONObject testObject = new JSONObject();
+        JSONObject[] answers = new JSONObject[4];
+        String id;
+        FileWriter writer = null;
+        for (int i = 0; i < 10; i++) {
+            id = UUID.randomUUID().toString();
+            File file = new File("src/main/resources/postJSON/" + id + ".json");
+            try {
+
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error occured: " + e);
+                throw new RuntimeException(e);
+            }
+            testObject.put("id", id);
+            testObject.put("question", "question number " + i);
+            for (int n = 0; n < 4; n++){
+                answers[n] = new JSONObject();
+                answers[n].put("username", "person number " + n);
+                answers[n].put("answer", "answer number " + n);
+            }
+            testObject.put("answers", answers);
+
+            try {
+                writer = new FileWriter(file);
+                writer.write(testObject.toString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
--- src/main/java/com/project/QAserver/QAserverApplication.java
+++ src/main/java/com/project/QAserver/QAserverApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,12 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QAserverApplication {
 
+	PostManager man = new PostManager();
 	public static void main(String[] args) {
 		SpringApplication.run(QAserverApplication.class, args);
 	}
-	@GetMapping
+	@GetMapping("/")
 	public String getListable(){
+		man.setupTest();
+		return "test";
+	}
+	@GetMapping("/next")
+	public String next(){
+		return "next";
+	}
+	@GetMapping("/previous")
+	public String previous(){
+		return "previous";
+	}
+	@GetMapping("/post/{id}")
+	public String viewPost(@PathVariable(value="id") String postID){
+		return "view post";
+	}
+	@GetMapping("/{postid}/{answer}/{description}/{username}")
+	public void addAnswer(@PathVariable(value="postid") String postID, @PathVariable(value = "answer") String answer, @PathVariable(value = "description") String description, @PathVariable(value = "username") String username){
 
 	}
+
 
 }
